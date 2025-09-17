744d06c71b5e780af6411df5b9df0e5983a29018
Reda Daghouj <mohamedredadaghouj@gmail.com>
2025-09-17 18:49:37 +0100

    start ex03

--- ex03/MateriaSource.hpp
+++ ex03/MateriaSource.hpp
@@ -3,13 +3,12 @@
 
 #include <string>
 #include "Materia.hpp"
+#include "IMateriaSource.hpp"
 
-class IMateriaSource
+class MateriaSource : public IMateriaSource
 {
 	public:
-		virtual ~IMateriaSource() {}
-		virtual void learnMateria(AMateria*) = 0;
-		virtual AMateria *createMateria(std::string const &type) = 0;
+		MateriaSource;
 };
 
 #endif
